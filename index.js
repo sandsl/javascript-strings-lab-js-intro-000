952ee946c0963a0b178041a2ca9192f3eec3ea88
sandslilyan@gmail.com <sandslilyan@gmail.com>
2018-04-21 18:21:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ var greetSpecialGuest = "" + specialGuest + "!";
 var topic = "Let's talk about space";
 
 var conversation = `${topic}`;
-`greeting +  ${specialGuest}! + ", " + topic "."
+
+`${greeting}! +  ${specialGuest}! + ", " + ${topic}! + "."`;
